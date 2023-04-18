01232c286c91364b22e1b1ef27acfae47ca8ed0e
shailesh <shailesh.d@simformsolutions.com>
2023-04-18 12:43:41 +0530

    Remove Duplication of Object in StringBuffer and StringBuilder

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,25 @@ public class Main {
 
         String str1 = " Shailesh  ";
         String str2 = new String("  Kanjibhai ");
+        System.out.println(str1.toUpperCase().trim());
+        System.out.println(str2.toUpperCase().trim());
 
-        StringBuffer name = new StringBuffer("  Shailesh   ");
-        name = new StringBuffer(name.toString().trim());
+        StringBuffer name = new StringBuffer("  Shailesh");
         name.append(",Mihir")
                 .append(",Parth")
                 .append(",Nimit")
                 .append(",Arpit")
                 .append(",Yash")
-                .append(",Likita");
+                .append(",Likita  ");
+        System.out.println(name.toString().toUpperCase().trim());
 
-        StringBuilder college = new StringBuilder("     LD College of Engineering  ");
-        college=new StringBuilder(college.toString().trim());
+        StringBuilder college = new StringBuilder("     LD College of Engineering");
         college.append(",VGEC")
                 .append(",Marvadi University")
                 .append(",L.J. Institute of Engineering & Technology")
                 .append(",Silver Oak University")
                 .append(",Charotar University of Science and Technology    ");
-
-        str1 = str1.toUpperCase().trim();
-        str2 = str2.toUpperCase().trim();
-
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println(name);
-        System.out.println(college);
+        System.out.println(college.toString().toUpperCase().trim());
 
     }
 }
